c2e2cc3d01419a5b1236c3a18adbcb7fa1ce235c
acorbat <agustin.corbat@ki.se>
2023-04-14 15:25:07 +0200

    Add utility functions to convert list of Molecules to pandas Dataframes and viceversa

--- src/trex/utils.py
+++ src/trex/utils.py
@@ -1,4 +1,8 @@
 import logging
+import pandas as pd
+from typing import List
+
+from ..molecule import Molecule
 
 
 class NiceFormatter(logging.Formatter):
@@ -13,3 +17,23 @@ class NiceFormatter(logging.Formatter):
         if record.levelno != logging.INFO:
             record.msg = "{}: {}".format(record.levelname, record.msg)
         return super().format(record)
+
+
+def molecule_list_2_df(mol_list: List[Molecule]) -> pd.DataFrame:
+    """Cast list of Molecules to pandas DataFrame"""
+    mol_dict = {'cell_id': [], 'umi': [], 'barcode': [], 'read_count': []}
+
+    for mol in mol_list:
+        mol_dict['cell_id'].append(mol.cell_id)
+        mol_dict['umi'].append(mol.umi)
+        mol_dict['barcode'].append(mol.clone_id)
+        mol_dict['read_count'].append(getattr(mol, 'read_count', -1))
+
+    return pd.DataFrame(mol_dict)
+
+
+def df_2_molecule_list(df: pd.DataFrame) -> List[Molecule]:
+    """Cast pandas DataFrame to list of Molecules"""
+    return [Molecule(umi=mol.umi, cell_id=mol.cell_id, clone_id=mol.barcode,
+                     read_count=mol.get('read_count', -1)
+                     ) for r, mol in df.iterrows()]
