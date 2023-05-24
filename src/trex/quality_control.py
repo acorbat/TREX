8b75ac31bac73ef6d359764b817835cc65548e86
acorbat <agustin.corbat@ki.se>
2023-05-24 11:30:31 +0200

    Add add_description as an option in qc plots

--- src/trex/quality_control.py
+++ src/trex/quality_control.py
@@ -33,7 +33,8 @@ def load_umi_count_matrix(data_dir: pathlib.Path):
     return pd.read_csv(UMI_DIR)
 
 
-def read_quality(reads: pd.DataFrame) -> plt.Axes:
+def read_quality(reads: pd.DataFrame,
+                 add_description : bool = True) -> plt.Axes:
     """Plot histogram of how many time a molecule was read."""
     count_reads = reads.groupby(['#cell_id', 'umi']).agg('count')
 
@@ -41,13 +42,15 @@ def read_quality(reads: pd.DataFrame) -> plt.Axes:
     plt.xlabel('Number of reads')
     plt.title('Number of reads per molecule')
 
-    txt = 'This plot shows how many times a molecule has been read. \n' \
-          'The more reads per molecule, the better.'
-    plt.text(0, -0.3, txt, transform=ax.transAxes, size=12)
+    if add_description:
+        txt = 'This plot shows how many times a molecule has been read. \n' \
+              'The more reads per molecule, the better.'
+        plt.text(0, -0.3, txt, transform=ax.transAxes, size=12)
     return ax
 
 
-def length_read(molecules: pd.DataFrame) -> plt.Axes:
+def length_read(molecules: pd.DataFrame,
+                add_description : bool = True) -> plt.Axes:
     """Plot histogram of how many bases were adequately read per barcode."""
     molecules['stripped_barcode'] = molecules.clone_id.apply(
         lambda x: re.sub("[-0]", "", x))
@@ -56,58 +59,69 @@ def length_read(molecules: pd.DataFrame) -> plt.Axes:
                       log=True)
     plt.xlabel('Number of detected bases')
     plt.title('Length of computed molecules')
-    txt = 'This plot shows how many bases have been read per molecule. \n' \
-          'Ideally all 30 bases have been read. If very few bases are read, \n'\
-          'we can not be sure how to complete the missing bases.'
-    plt.text(0, -0.3, txt, transform=ax.transAxes, size=12)
+
+    if add_description:
+        txt = 'This plot shows how many bases have been read per molecule. \n' \
+              'Ideally all 30 bases have been read. If very few bases are read, \n'\
+              'we can not be sure how to complete the missing bases.'
+        plt.text(0, -0.3, txt, transform=ax.transAxes, size=12)
     return ax
 
 
-def molecules_per_cell(molecules: pd.DataFrame) -> plt.Axes:
+def molecules_per_cell(molecules: pd.DataFrame,
+                       add_description : bool = True) -> plt.Axes:
     """Plot histogram of how many molecules were detected per cell."""
     count_reads = molecules.groupby(['#cell_id']).umi.agg('count')
 
     ax = sns.histplot(count_reads.values, discrete=True, log=True)
     plt.xlabel('Molecules per cell')
     plt.title('Number of molecules')
-    txt = 'This plot shows how many molecules were found per cell. \n' \
-          'Cell that have a single molecule might be filtered out later. \n' \
-          'Cells that have too many might be result of non-removed \n doublets.'
-    plt.text(0, -0.3, txt, transform=ax.transAxes, size=12)
+
+    if add_description:
+        txt = 'This plot shows how many molecules were found per cell. \n' \
+              'Cell that have a single molecule might be filtered out later. \n' \
+              'Cells that have too many might be result of non-removed \n doublets.'
+        plt.text(0, -0.3, txt, transform=ax.transAxes, size=12)
     return ax
 
 
-def molecules_per_barcode(molecules: pd.DataFrame) -> plt.Axes:
+def molecules_per_barcode(molecules: pd.DataFrame,
+                          add_description : bool = True) -> plt.Axes:
     """Plot histogram of how many molecules were detected per viral barcode."""
     count_reads = molecules.groupby(['clone_id']).umi.agg('count')
 
     ax = sns.histplot(count_reads.values, discrete=True, log=True)
     plt.xlabel('Molecules per barcode')
     plt.title('Number of molecules')
-    txt = 'This plot shows how many molecules were found per barcode. \n' \
-          'Barcodes that appear a few times might not be found in more \n' \
-          'cells. Barcodes that have too many might be result of \n' \
-          'contamination or alignment problems or big clones.'
-    plt.text(0, -0.3, txt, transform=ax.transAxes, size=12)
+
+    if add_description:
+        txt = 'This plot shows how many molecules were found per barcode. \n' \
+              'Barcodes that appear a few times might not be found in more \n' \
+              'cells. Barcodes that have too many might be result of \n' \
+              'contamination or alignment problems or big clones.'
+        plt.text(0, -0.3, txt, transform=ax.transAxes, size=12)
     return ax
 
 
-def unique_barcodes_per_cell(molecules: pd.DataFrame) -> plt.Axes:
+def unique_barcodes_per_cell(molecules: pd.DataFrame,
+                             add_description : bool = True) -> plt.Axes:
     """Plot histogram of how many unique barcodes were detected per cell."""
     count_reads = molecules.groupby('#cell_id').clone_id.unique().apply(len)
 
     ax = sns.histplot(count_reads.values, discrete=True, log=True)
     plt.xlabel('Unique barcodes per cell')
     plt.title('Number of unique barcodes')
-    txt = 'This plot shows how many unique barcodes were detected per cell.\n' \
-          'Cells with many unique barcodes show either lots of infection \n' \
-          'events or possible unfiltered doublets.'
-    plt.text(0, -0.3, txt, transform=ax.transAxes, size=12)
+
+    if add_description:
+        txt = 'This plot shows how many unique barcodes were detected per cell.\n' \
+              'Cells with many unique barcodes show either lots of infection \n' \
+              'events or possible unfiltered doublets.'
+        plt.text(0, -0.3, txt, transform=ax.transAxes, size=12)
     return ax
 
 
 def hamming_distance_histogram(molecules: pd.DataFrame,
-                               ignore_incomplete=True) -> plt.Axes:
+                               ignore_incomplete: bool = True) -> plt.Axes:
     """Plot histogram of Hamming distance between barcodes. ignore_incomplete is
      set to True by default and it removes incomplete barcodes."""
     if ignore_incomplete:
